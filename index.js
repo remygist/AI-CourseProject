085adc66bdc1c8714d472b75fbe9c30fb0d0cde2
remygist <remy.gistelinck@student.ehb.be>
2024-05-13 22:19:11 +0200

    Refactor commitMessage function in index.js
    -m
    
    Simplify the commitMessage function by removing commented out code and unnecessary console.log statements

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+const fs = require('fs')
 const simpleGit = require("simple-git");
 const git = simpleGit.default();
 const OpenAI = require("openai");
+const { isUtf8 } = require('buffer');
 
 const openaiClient = new OpenAI({
     apiKey: "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",
 });
+
+const bestPractices = fs.readFileSync('best-practices.txt', 'utf-8')
 
 async function getDiff() {
     const diff = await git.diff();
@@ -14,7 +18,11 @@ async function getDiff() {
 
 async function connectAssistant(diff) {
     const content = `You are an assistant made to resume changes in code. You will have the results of a git diff command and you will have to summarize the changes that have been made to the code. 
-    If there are no changes, you have to tell it. You can not make up changes that haven't been made. You have to follow the best practices for creating git commit messages. 
+    If there are no changes, you have to tell it. You can not make up changes that haven't been made. 
+
+    You have to follow these best practices for creating git commit messages: 
+    ${bestPractices}
+    
     You can't say anything else than what the commit message should be, and if needed the description. 
 
     You only output JSON, in the following format:
